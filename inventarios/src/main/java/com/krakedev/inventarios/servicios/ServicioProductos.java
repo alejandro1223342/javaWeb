9f293545bd24a2409f6b9387a42f198dfe3f94a7
alejandro1223342 <alejomuoss@gmail.com>
2024-11-18 11:18:27 -0500

    Se implementa la actualizacion de productos

--- inventarios/src/main/java/com/krakedev/inventarios/servicios/ServicioProductos.java
+++ inventarios/src/main/java/com/krakedev/inventarios/servicios/ServicioProductos.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,9 +13,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.krakedev.inventarios.bdd.ProductosBDD;
-import com.krakedev.inventarios.bdd.ProveedoresBDD;
 import com.krakedev.inventarios.entidades.Producto;
-import com.krakedev.inventarios.entidades.Proveedor;
 import com.krakedev.inventarios.excepciones.KrakeDevException;
 
 @Path("productos")
@@ -53,5 +52,21 @@ public class ServicioProductos {
 			return Response.serverError().build();
 		}
 
+	}
+	
+	@Path("actualizar")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response actualizar(Producto productos) {
+
+		System.out.println("ACTUALIZANDO CLIENTE>>>>" + productos);
+		ProductosBDD pro = new ProductosBDD();
+		try {
+			pro.actualizar(productos);
+			return Response.ok().build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
 	}
 }
